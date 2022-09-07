1e5fcc77dbee605f719b1f668527b0c265da25d9
Parkseonho <tjsgh2103@gmail.com>
2022-09-07 10:34:30 +0900

    todoList 관련 추가

---
+++ src/main/java/com/example/controller/TodoController.java
@@ -0,0 +1,56 @@
+package com.example.controller;
+
+import com.example.domain.Todo;
+import com.example.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+
+@RestController()
+@RequestMapping(value = "/todos")
+public class TodoController {
+
+    private final TodoService todoService;
+
+    TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping("")
+    public List<Todo> getTodos(){
+        return todoService.getTodos();
+    }
+
+    @PostMapping("")
+    public List<Todo> createTodos(@RequestBody Todo todo){
+
+        todoService.createTodo(todo);
+        return todoService.getTodos();
+    }
+
+    @DeleteMapping("{id}")
+    public List<Todo> deleteTodos(@PathVariable("id") Integer id, HttpServletResponse response) throws IOException {
+
+        System.out.println("id: " + id);
+        todoService.deleteTodo(id, response);
+        return todoService.getTodos();
+    }
+
+    @PatchMapping("{id}")
+    public List<Todo> checkTodo(@PathVariable("id") Integer id, HttpServletResponse response) throws IOException {
+        todoService.checkTodo(id, response);
+        return todoService.getTodos();
+    }
+
+    @PatchMapping("/edit/{id}")
+    public List<Todo> editTodoById(@PathVariable("id") Integer id, @RequestBody Map<String, Object> params, HttpServletResponse response) throws IOException {
+        todoService.editTodoById(id, params.get("content"), response);
+        return todoService.getTodos();
+    }
+}
+
+
---
+++ src/main/java/com/example/service/TodoService.java
@@ -0,0 +1,85 @@
+package com.example.service;
+
+import com.example.dao.TodoRepository;
+import com.example.domain.Todo;
+import com.example.dto.ResponseDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class TodoService {
+    private final TodoRepository todoRepository;
+
+    public Todo createTodo(Todo todo) {
+        Todo newTodo = new Todo();
+        newTodo.setContent(todo.getContent());
+        newTodo.setChecked(false);
+        todoRepository.save(newTodo);
+
+        return newTodo;
+    }
+
+    public List<Todo> getTodos() {
+        return todoRepository.findAll();
+    }
+
+
+    public void deleteTodo(Integer id, HttpServletResponse response) throws IOException {
+        Optional<Todo> todo = todoRepository.findById(id);
+        if (todo.isPresent()) {
+            todoRepository.deleteById(id);
+        } else {
+            ResponseDTO responseDTO = new ResponseDTO();
+            responseDTO.setCode("F-1");
+            responseDTO.setMessage("이미 삭제된 할 일입니다.");
+            response.setStatus(404);
+            response.setHeader("content-type", "application/json;charset=utf-8");
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writeValue(response.getOutputStream(), responseDTO);
+
+        }
+    }
+
+    public void checkTodo(Integer id, HttpServletResponse response) throws IOException {
+        Optional<Todo> todo = todoRepository.findById(id);
+        if(todo.isPresent()) {
+            Todo targetTodo = todo.get();
+            targetTodo.setChecked(!targetTodo.getChecked());
+            todoRepository.save(targetTodo);
+        } else {
+            ResponseDTO responseDTO = new ResponseDTO();
+            responseDTO.setCode("F-2");
+            responseDTO.setMessage("없는 할 일 입니다.");
+            response.setStatus(404);
+            response.setHeader("content-type", "application/json;charset=utf-8");
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writeValue(response.getOutputStream(), responseDTO);
+        }
+
+
+    }
+
+    public void editTodoById(Integer id, Object content, HttpServletResponse response) throws IOException {
+        Optional<Todo> targetTodo = todoRepository.findById(id);
+        if(targetTodo.isPresent()) {
+            Todo todo = targetTodo.get();
+            todo.setContent((String)content);
+            todoRepository.save(todo);
+        } else {
+            ResponseDTO responseDTO = new ResponseDTO();
+            responseDTO.setCode("F-3");
+            responseDTO.setMessage("없는 할 일 입니다.");
+            response.setStatus(404);
+            response.setHeader("content-type", "application/json;charset=utf-8");
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writeValue(response.getOutputStream(), responseDTO);
+        }
+    }
+}
